aefced3a368c1c559b2c704afd83c9841c8e06a3
Mehmet ALPEREN <me.alperen@gmail.com>
2021-11-26 17:41:22 +0300

    Toggle and delete operations finished.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggle, destroy } from '../redux/todos/todosSlice';
 
 const TodoList = () => {
   const items = useSelector((state) => state.todos.items);
-  console.log(items);
+
+  const dispatch = useDispatch();
+
+  const handleDestroy = (id) => {
+    if (window.confirm('Are you sure?')) {
+      dispatch(destroy(id));
+    }
+  };
 
   return (
     <ul className='todo-list'>
       {items.map((item) => (
         <li key={item.id} className={item.completed ? 'completed' : ''}>
           <div className='view'>
-            <input className='toggle' type='checkbox' />
+            <input
+              className='toggle'
+              type='checkbox'
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
-            <button className='destroy'></button>
+            <button
+              className='destroy'
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -8,7 +8,22 @@ export const todosSlice = createSlice({
       { id: '2', title: 'Read a book', completed: false },
     ],
   },
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed;
+    },
+    destroy: (state, action) => {
+      const id = action.payload;
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
+    },
+  },
 });
 
+export const { addTodo, toggle, destroy } = todosSlice.actions;
 export default todosSlice.reducer;
